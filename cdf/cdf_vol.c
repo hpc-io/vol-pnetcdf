3a086a342b6170b6c1068c0f387e0681ce1aa97f
Rick <rzamora@anl.gov>
2019-03-06 17:25:23 -0600

    adding functionality to read CDF header in CDF VOL

--- cdf/cdf_vol.c
+++ cdf/cdf_vol.c
@@ -37,7 +37,10 @@
 
 /* The CDF VOL info object */
 typedef struct H5VL_cdf_t {
-    int  fd; /* posix file object */
+	int  fd; /* posix file object */
+	uint8_t fmt; /* CDF File Spec (1,2,5) */
+	uint64_t numrecs; /* length of record dimension */
+	uint64_t ndims; /* number of dimensions */
 } H5VL_cdf_t;
 
 /********************* */
@@ -47,6 +50,12 @@ typedef struct H5VL_cdf_t {
 /* Helper routines */
 static H5VL_cdf_t *H5VL_cdf_new_obj(int fd);
 static herr_t H5VL_cdf_free_obj(H5VL_cdf_t *obj);
+static uint32_t bytestr_to_uint32_t(char *bytestr);
+static uint64_t bytestr_to_uint64_t(char *bytestr);
+static uint32_t get_uint32_t(H5VL_cdf_t* file, off_t *rdoff);
+static uint64_t get_uint64_t(H5VL_cdf_t* file, off_t *rdoff);
+static uint64_t get_non_neg(H5VL_cdf_t* file, off_t *rdoff);
+static uint8_t get_cdf_spec(H5VL_cdf_t* file, off_t *rdoff);
 
 /* "Management" callbacks */
 static herr_t H5VL_cdf_init(hid_t vipl_id);
@@ -165,6 +174,69 @@ const H5VL_class_t H5VL_cdf_g = {
 /* The connector identification number, initialized at runtime */
 static hid_t H5VL_CDF_g = H5I_INVALID_HID;
 
+
+static uint32_t
+bytestr_to_uint32_t(char *bytestr)
+{
+	/* Note: Little Endian Case */
+	//uint32_t myInt1 = (uint32_t)bytestr[0] + ((uint32_t)bytestr[1] << 8) + ((uint32_t)bytestr[2] << 16) + ((uint32_t)bytestr[3] << 24);
+	/* Big Endian Case */
+	uint32_t myInt1 = ((uint32_t)bytestr[0] << 24) + ((uint32_t)bytestr[1] << 16) + ((uint32_t)bytestr[2] << 8) + (uint32_t)bytestr[3];
+	/* Print Result */
+	//printf("uint32_t value = %d (bytes = %d %d %d %d) \n", myInt1,(uint8_t)bytestr[0],(uint8_t)bytestr[1],(uint8_t)bytestr[2],(uint8_t)bytestr[3] );
+	return myInt1;
+}
+
+
+static uint64_t
+bytestr_to_uint64_t(char *bytestr)
+{
+	/* Big Endian Case */
+	uint64_t myInt1 = ((uint64_t)bytestr[0] << 56) + ((uint64_t)bytestr[1] << 48) + ((uint64_t)bytestr[2] << 40) + ((uint64_t)bytestr[3] << 32) +  ((uint64_t)bytestr[4] << 24) + ((uint64_t)bytestr[5] << 16) + ((uint64_t)bytestr[6] << 8) + (uint64_t)bytestr[7];
+	/* Print Result */
+	//printf("uint32_t value = %llu (bytes = %d %d %d %d %d %d %d %d) \n", myInt1,(uint8_t)bytestr[0],(uint8_t)bytestr[1],(uint8_t)bytestr[2],(uint8_t)bytestr[3],(uint8_t)bytestr[4],(uint8_t)bytestr[5],(uint8_t)bytestr[6],(uint8_t)bytestr[7] );
+	return myInt1;
+}
+
+static uint32_t
+get_uint32_t(H5VL_cdf_t* file, off_t *rdoff)
+{
+	char bytestr[4];
+	pread(file->fd, &bytestr[0], 4, *rdoff); *rdoff = *rdoff + 4;
+	return (uint64_t)bytestr_to_uint32_t( bytestr );
+}
+
+static uint64_t
+get_uint64_t(H5VL_cdf_t* file, off_t *rdoff)
+{
+	char bytestr[8];
+	pread(file->fd, &bytestr[0], 8, *rdoff); *rdoff = *rdoff + 8;
+	return bytestr_to_uint64_t( bytestr );
+}
+
+static uint8_t
+get_cdf_spec(H5VL_cdf_t* file, off_t *rdoff)
+{
+	char bytestr[4];
+	pread(file->fd, &bytestr[0], 4, *rdoff); *rdoff = *rdoff + 4;
+	file->fmt = (uint8_t) bytestr[3];
+	printf("------- (FILE FORMAT = %c%c%c-%d) \n", bytestr[0], bytestr[1], bytestr[2], (uint8_t) bytestr[3]);
+	return (uint8_t) bytestr[3];
+}
+
+static uint64_t
+get_non_neg(H5VL_cdf_t* file, off_t *rdoff)
+{
+	/* Read length of record dimension (numrecs) */
+	if (file->fmt < 5) {
+		/* NON_NEG = <non-negative INT> = <32-bit signed integer, Bigendian, two's complement> */
+		return (uint64_t) get_uint32_t(file,rdoff);
+	} else {
+		/* NON_NEG = <non-negative INT64> = <64-bit signed integer, Bigendian, two's complement> */
+		return get_uint64_t(file,rdoff);
+	}
+}
+
 /*-------------------------------------------------------------------------
  * Function:    H5VL_cdf_new_obj
  *
@@ -181,6 +253,9 @@ H5VL_cdf_new_obj(int fd)
 
     new_obj = (H5VL_cdf_t *)calloc(1, sizeof(H5VL_cdf_t));
     new_obj->fd = fd;
+    new_obj->fmt = 0;
+    new_obj->numrecs = 0;
+    new_obj->ndims = 0;
 
     return new_obj;
 } /* end H5VL__cdf_new_obj() */
@@ -287,15 +362,50 @@ static void *
 H5VL_cdf_file_open(const char *name, unsigned flags, hid_t fapl_id,
     hid_t dxpl_id, void **req)
 {
-    H5VL_cdf_info_t *info;
-    H5VL_cdf_t *file;
+		H5VL_cdf_info_t *info;
+		H5VL_cdf_t *file;
+		off_t rdoff = 0;
+		char bytes_sp[4];
+		char bytes_dp[8];
 
-    printf("------- CDF VOL FILE Open\n");
+		printf("------- CDF VOL FILE Open\n");
 
-    /* For now - Just using POSIX to prototype/test basic VOL */
-    file = H5VL_cdf_new_obj( open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR) );
+		/* For now - Just using POSIX to prototype/test basic VOL */
+		file = H5VL_cdf_new_obj( open(name, O_RDONLY) );
 
-    return (void *)file;
+		/* Read "magic" part of header */
+		get_cdf_spec(file, &rdoff);
+
+		/* Read length of record dimension (numrecs) */
+		file->numrecs = (uint64_t) get_non_neg(file, &rdoff);
+		/* Note: Above read will be "STREAMING" rather than "NON_NEG" if all bytes are \xFF */
+
+		/* Read dimension list (dim_list) */
+
+		/* First check if first 4 bytes are 0 */
+		uint32_t nc_dim = get_uint32_t(file, &rdoff);
+		if ( nc_dim > (uint32_t)0 ){
+
+			/* Make Sure these 4 bytes are "NC_DIMENSION" */
+			if ( nc_dim != (uint32_t)10 ) {
+				printf("ERROR - Expecting NC_DIMENSION (%d).\n", nc_dim);
+			} else {
+				//printf("GOOD - NC_DIMENSION (%d).\n", nc_dim);
+			}
+
+			/* Get nelms of dim_list (file->ndims) */
+			file->ndims = (uint64_t) get_non_neg(file, &rdoff);
+			printf("ndims = %llu\n", file->ndims);
+
+		} else {
+			/* dim_list is ABSENT - Move rdoff by 4|8 bytes */
+			if (file->fmt < 5)
+				rdoff = rdoff + 4;
+			else
+				rdoff = rdoff + 8;
+		}
+
+		return (void *)file;
 } /* end H5VL_cdf_file_open() */
 
 
--- cdf/vol_test.c
+++ cdf/vol_test.c
@@ -4,7 +4,7 @@
 #include <assert.h>
 #include "hdf5.h"
 #include "cdf_vol.h"
-#define FILE "testfile.h5"
+#define FILE "testfile.nc"
 #include <pnetcdf.h>
 
 
@@ -16,67 +16,156 @@ static void handle_error(int status, int lineno)
 
 
 int write_cdf( ) {
-    int ret, ncid=0, nprocs, rank, dimid, varid1=0, varid2=0, ndims=1;
+    int ret, ncfile, nprocs, rank, dimid, varid1, varid2, ndims=1;
     char filename[256], buf[13] = "Hello World\n";
+		MPI_Offset start, count=1;
+    int data;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    strcpy(filename, FILE);
+
+    ret = ncmpi_create(MPI_COMM_WORLD, filename, NC_CLOBBER|NC_64BIT_OFFSET, MPI_INFO_NULL, &ncfile);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    ret = ncmpi_def_dim(ncfile, "d1", nprocs, &dimid);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    ret = ncmpi_def_var(ncfile, "v1", NC_INT, ndims, &dimid, &varid1);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    ret = ncmpi_def_var(ncfile, "v2", NC_INT, ndims, &dimid, &varid2);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    ret = ncmpi_put_att_text(ncfile, NC_GLOBAL, "string", 13, buf);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    /* All processors defined the dimensions, attributes, and variables,
+     * but here in ncmpi_enddef is the one place where metadata I/O
+     * happens.  Behind the scenes, rank 0 takes the information and writes
+     * the netcdf header.  All processes communicate to ensure they have
+     * the same (cached) view of the dataset */
+
+    ret = ncmpi_enddef(ncfile);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    start=rank, count=1, data=rank;
+
+    /* in this simple example every process writes its rank to two 1d variables */
+    ret = ncmpi_put_vara_int_all(ncfile, varid1, &start, &count, &data);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    ret = ncmpi_put_vara_int_all(ncfile, varid2, &start, &count, &data);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    ret = ncmpi_close(ncfile);
+		if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    return 0;
+}
+
+int read_cdf( ) {
+
+    int i, j, rank, nprocs, ret;
+    int ncfile, ndims, nvars, ngatts, unlimited;
+    int var_ndims, var_natts;;
+    MPI_Offset *dim_sizes, var_size;
+    MPI_Offset *start, *count;
+    char filename[256], varname[NC_MAX_NAME+1];
+    int *dimids=NULL;
+    nc_type type;
     int *data=NULL;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    strcpy(filename, "testfile.nc");
+    strcpy(filename, FILE);
 
-    if (rank == 0) {
-        ret = ncmpi_create(MPI_COMM_SELF, filename,
-                           NC_CLOBBER|NC_64BIT_OFFSET, MPI_INFO_NULL, &ncid);
+    ret = ncmpi_open(MPI_COMM_WORLD, filename, NC_NOWRITE, MPI_INFO_NULL,
+                     &ncfile);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    /* reader knows nothing about dataset, but we can interrogate with query
+     * routines: ncmpi_inq tells us how many of each kind of "thing"
+     * (dimension, variable, attribute) we will find in the file  */
+
+    /* no communication needed after ncmpi_open: all processors have a cached
+     * view of the metadata once ncmpi_open returns */
+
+    ret = ncmpi_inq(ncfile, &ndims, &nvars, &ngatts, &unlimited);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+
+    /* we do not really need the name of the dimension or the variable for
+     * reading in this example.  we could, in a different example, take the
+     * name of a variable on the command line and read just that one */
+
+    dim_sizes = (MPI_Offset*) calloc(ndims, sizeof(MPI_Offset));
+    /* netcdf dimension identifiers are allocated sequentially starting
+     * at zero; same for variable identifiers */
+    for(i=0; i<ndims; i++)  {
+        ret = ncmpi_inq_dimlen(ncfile, i, &(dim_sizes[i]) );
         if (ret != NC_NOERR) handle_error(ret, __LINE__);
-
-        ret = ncmpi_def_dim(ncid, "d1", nprocs, &dimid);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
-
-        ret = ncmpi_def_var(ncid, "v1", NC_INT, ndims, &dimid, &varid1);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
-
-        ret = ncmpi_def_var(ncid, "v2", NC_INT, ndims, &dimid, &varid2);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
-
-        ret = ncmpi_put_att_text(ncid, NC_GLOBAL, "string", 13, buf);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
-
-        ret = ncmpi_enddef(ncid);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
-
-        /* first reason this approach is not scalable:  need to allocate
-        * enough memory to hold data from all processors */
-        data = (int*)calloc(nprocs, sizeof(int));
     }
 
-    /* second reason this approach is not scalable: sending to rank 0
-     * introduces a serialization point, even if using an optimized
-     * collective routine */
-    MPI_Gather(&rank, 1, MPI_INT, data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    for(i=0; i<nvars; i++) {
+        /* obtain the number of dimensions of variable i, so we can allocate
+         * the dimids array */
+        ret = ncmpi_inq_varndims(ncfile, i, &var_ndims);
+        if (ret != NC_NOERR) handle_error(ret, __LINE__);
+        dimids = (int*) malloc(var_ndims * sizeof(int));
 
-    if (rank == 0) {
-        /* and lastly, the third reason this approach is not scalable: I/O
-         * happens from a single processor.  This approach can be ok if the
-         * amount of data is quite small, but almost always the underlying
-         * MPI-IO library can do a better job */
-        MPI_Offset start[1], count[1];
-        start[0]=0, count[0]=nprocs;
-        ret = ncmpi_put_vara_int_all(ncid, varid1, start, count, data);
+        /* much less coordination in this case compared to rank 0 doing all
+         * the i/o: everyone already has the necessary information */
+        ret = ncmpi_inq_var(ncfile, i, varname, &type, &var_ndims, dimids,
+                &var_natts);
         if (ret != NC_NOERR) handle_error(ret, __LINE__);
 
-        ret = ncmpi_put_vara_int_all(ncid, varid2, start, count, data);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
+        start = (MPI_Offset*) calloc(var_ndims, sizeof(MPI_Offset));
+        count = (MPI_Offset*) calloc(var_ndims, sizeof(MPI_Offset));
 
-        ret = ncmpi_close(ncid);
-        if (ret != NC_NOERR) handle_error(ret, __LINE__);
+        /* we will simply decompose along one dimension.  Generally the
+         * application has some algorithm for domain decomposition.  Note
+         * that data decomposition can have an impact on i/o performance.
+         * Often it's best just to do what is natural for the application,
+         * but something to consider if performance is not what was
+         * expected/desired */
 
-        free(data);
+        start[0] = (dim_sizes[dimids[0]]/nprocs)*rank;
+        count[0] = (dim_sizes[dimids[0]]/nprocs);
+        var_size = count[0];
+
+        for (j=1; j<var_ndims; j++) {
+            start[j] = 0;
+            count[j] = dim_sizes[dimids[j]];
+            var_size *= count[j];
+        }
+
+        switch(type) {
+            case NC_INT:
+                data = (int*) calloc(var_size, sizeof(int));
+                ret = ncmpi_get_vara_all(ncfile, i, start, count, data,
+                        var_size, MPI_INT);
+                free(data);
+                if (ret != NC_NOERR) handle_error(ret, __LINE__);
+                break;
+            default:
+                /* we can do this for all the known netcdf types but this
+                 * example is already getting too long  */
+                fprintf(stderr, "unsupported NetCDF type \n");
+        }
+
+        free(start);
+        free(count);
+        free(dimids);
     }
+
+    ret = ncmpi_close(ncfile);
+    if (ret != NC_NOERR) handle_error(ret, __LINE__);
+    free(dim_sizes);
 
     return 0;
 }
-
 
 int main(int argc, char **argv) {
 
@@ -102,6 +191,9 @@ int main(int argc, char **argv) {
 
 	/* Write File with pNetCDF */
 	write_cdf();
+
+	/* Read File with pNetCDF */
+	read_cdf();
 
 	fapl = H5Pcreate (H5P_FILE_ACCESS);
 
